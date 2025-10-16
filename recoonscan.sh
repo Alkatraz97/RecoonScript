1dd5d9053c152fd3f490a6b3b103f83635ae9519
Alkatraz97 <46528079+Alkatraz97@users.noreply.github.com>
2025-10-16 07:02:15 -0400

    Adding Eyewitness tool for scan web page

--- recoonscan.sh
+++ recoonscan.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Nome: scan_hosts.sh
+# Descrizione: Scansiona tutte le porte degli host elencati nel file 'hosts'
 
 DATE_FORMATTED=$(date +"%d:%m:%Y %H:%M")
 echo "Inizio sansione PTAC8k $DATE_FORMATTED" >> log_recoonscan.log
@@ -56,7 +58,7 @@ for i in $(cat hosts.txt); do
             echo "$https_ports" > $i/https_ports.txt
             for port in $https_ports; do
                 mkdir -p $i/nuclei
-                #nuclei -target $i:$port -o "$i/nuclei/$port""_http_nuclei.txt"
+                nuclei -target $i:$port -o "$i/nuclei/$port""_http_nuclei.txt"
             done
             echo "--- END nuclei https can of $i ---" >> log_recoonscan.log
         fi
@@ -69,7 +71,7 @@ for i in $(cat hosts.txt); do
             echo "$http_ports" > $i/http_ports.txt
             for port in $http_ports; do
                 mkdir -p $i/nuclei
-                #nuclei -target $i:$port -o "$i/nuclei/$port""_http_nuclei.txt"
+                nuclei -target $i:$port -o "$i/nuclei/$port""_http_nuclei.txt"
             done
             echo "--- END nuclei http scan of $i ---" >> log_recoonscan.log
         fi	
@@ -82,8 +84,14 @@ for i in $(cat hosts.txt); do
         if [ -n "$ports_string" ]; then
             echo "--- START Advanced nmap scan $i ---" >> log_recoonscan.log
             nmap -sC -sV -p $ports_string $i -vv -oN "$i/nmap/$i.nmap"
-            
             echo "--- END Advanced nmap scan  $i ---" >> log_recoonscan.log
+            
+            echo "--- START Eyewitness scan $i ---" >> log_recoonscan.log
+            echo $i > temp_$i
+            eyewitness -f temp_$i -d $i/eyewitness --only-ports $ports_string --timeout 1 --no-prompt
+            rm temp_$i
+            echo "--- END Eyewitness scan $i ---" >> log_recoonscan.log
+            
         fi
 
         
@@ -95,6 +103,6 @@ for i in $(cat hosts.txt); do
     		
 done
 
-# check porte http --> httpx-toolkit -sc -ct -cl -title -server -l Desktop/recoonscan/hosts.txt -p 8080 
-
 echo "----------- END SCAN, happy hack! -----------"
+
+
